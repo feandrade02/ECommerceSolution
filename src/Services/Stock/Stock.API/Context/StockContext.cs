5b796e9291b4a16155fb5996d47bac383eb6e87d
feandrade02 <fe.a.andrade@hotmail.com>
2025-09-30 14:10:07 -0300

    build: configurando arquivos de build, banco de dados e gitignore

---
+++ src/Services/Stock/Stock.API/Context/StockContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Stock.Domain.Entities;
+
+namespace Stock.Context
+{
+    public class StockContext : DbContext
+    {
+        public StockContext(DbContextOptions<StockContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().Property(p => p.Preco).HasPrecision(18, 2);
+        }
+    }
+}
