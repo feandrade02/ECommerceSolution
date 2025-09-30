739ca9d72e2d3d4b330bc13206bd1507ea24c4ab
feandrade02 <fe.a.andrade@hotmail.com>
2025-09-30 14:10:22 -0300

    feat: add entidade produto

---
+++ src/Services/Stock/Stock.API/Domain/Entities/Produto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stock.Domain.Entities
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int QuantidadeEstoque { get; set; }
+    }
+}
---
+++ src/Services/Stock/Stock.API/Migrations/20250930170326_CriacaoTabelaProduto.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stock.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelaProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    QuantidadeEstoque = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
