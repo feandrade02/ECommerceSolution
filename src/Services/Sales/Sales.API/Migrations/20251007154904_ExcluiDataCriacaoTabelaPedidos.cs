bc8d7d1f8cbcbd1ab315377b8e7ee3caba6cbb96
feandrade02 <fe.a.andrade@hotmail.com>
2025-10-07 16:04:53 -0300

    build: novas migrations para alteração em coluna da tabela de pedidos do banco de dados

---
+++ src/Services/Sales/Sales.API/Migrations/20251007154904_ExcluiDataCriacaoTabelaPedidos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sales.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExcluiDataCriacaoTabelaPedidos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCriacao",
+                table: "Pedidos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeProduto",
+                table: "ItensPedido",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Pedidos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeProduto",
+                table: "ItensPedido",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
