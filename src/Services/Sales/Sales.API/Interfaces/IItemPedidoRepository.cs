76141d48f5045b647ece270613c057811ecf67c2
feandrade02 <fe.a.andrade@hotmail.com>
2025-10-07 19:37:50 -0300

    feat: add camada de repositorio e suas interfaces para as entidades de Pedido e ItemPedido

---
+++ src/Services/Sales/Sales.API/Interfaces/IItemPedidoRepository.cs
@@ -0,0 +1,29 @@
+using Sales.API.Domain.Entities;
+
+namespace Sales.API.Interfaces
+{
+    public interface IItemPedidoRepository
+    {
+        Task<List<ItemPedido>> GetAllItemPedidosAsync(
+            int page = 1,
+            int pageSize = 10,
+            string nomeProduto = null,
+            string sortBy = null,
+            bool ascending = true,
+            int? minPrecoUnitario = null,
+            int? maxPrecoUnitario = null,
+            int? minQuantidade = null,
+            int? maxQuantidade = null
+        );
+
+        Task<ItemPedido> GetItemPedidoByIdAsync(int id);
+
+        Task AddItemPedidoAsync(ItemPedido itemPedido);
+
+        Task UpdateItemPedidoAsync(ItemPedido itemPedido);
+
+        Task DeleteItemPedidoAsync(ItemPedido itemPedido);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ src/Services/Sales/Sales.API/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,28 @@
+using Sales.API.Domain.Entities;
+using Sales.API.Domain.Enums;
+
+namespace Sales.API.Interfaces
+{
+    public interface IPedidoRepository
+    {
+        Task<List<Pedido>> GetAllPedidosAsync(
+            int page = 1,
+            int pageSize = 10,
+            string sortBy = null,
+            bool ascending = true,
+            StatusPedido? status = null,
+            int? minTotalValue = null,
+            int? maxTotalValue = null
+        );
+
+        Task<Pedido> GetPedidoByIdAsync(int id);
+
+        Task AddPedidoAsync(Pedido pedido);
+
+        Task UpdatePedidoAsync(Pedido pedido);
+
+        Task DeletePedidoAsync(Pedido pedido);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
