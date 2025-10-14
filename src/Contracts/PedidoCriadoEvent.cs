aadb58e04400b965557fb59cc8f3e320c5d3144d
feandrade02 <fe.a.andrade@hotmail.com>
2025-10-14 17:48:17 -0300

    feat: criação de Contracts para estruturar a publicação de eventos no rabbitmq

---
+++ src/Contracts/PedidoCriadoEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Contracts;
+
+public record PedidoCriadoEvent
+{
+    public Guid CorrelationId { get; init; }    
+    public List<ItemPedidoReferenceDTO> Itens { get; init; }
+}
+
+public record ItemPedidoReferenceDTO
+{
+    public int IdProduto { get; init; }
+    public int Quantidade { get; init; }
+}
