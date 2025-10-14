ec2197ef99f02b657b1488ca3d1043ec0f20eb4d
feandrade02 <fe.a.andrade@hotmail.com>
2025-10-14 18:00:46 -0300

    refactor: nova forma de validação no endpoint ObterTodos utilizando FluentValidation

--- src/Sales/Sales.API/Controllers/PedidoController.cs
+++ src/Sales/Sales.API/Controllers/PedidoController.cs
@@ -1,161 +1,113 @@
 using Microsoft.AspNetCore.Mvc;
 using Sales.API.Domain.DTOs;
-using Sales.API.Domain.Enums;
 using Sales.API.Domain.Interfaces;
 using Sales.API.Domain.ModelViews;
 
-namespace Sales.API.Controllers
+namespace Sales.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PedidoController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class PedidoController : ControllerBase
+    private readonly IPedidoService _pedidoService;
+    private readonly ILogger<PedidoController> _logger;
+
+    public PedidoController(IPedidoService pedidoService, ILogger<PedidoController> logger)
     {
-        private readonly IPedidoRepository _pedidoRepository;
-        private readonly IPedidoService _pedidoService;
-        private readonly ILogger<PedidoController> _logger;
+        _pedidoService = pedidoService;
+        _logger = logger;
+    }
 
-        public PedidoController(IPedidoRepository pedidoRepository, IPedidoService pedidoService, ILogger<PedidoController> logger)
+    [HttpGet("ObterTodos")]
+    public async Task<IActionResult> GetAllPedidos([FromQuery] GetAllPedidosDTO request)
+    {
+        try
         {
-            _pedidoRepository = pedidoRepository;
-            _pedidoService = pedidoService;
-            _logger = logger;
+            var pedidos = await _pedidoService.GetAllPedidosAsync(request);
+
+            var pedidosModelView = pedidos.Select(p => new PedidoModelView
+            {
+                Id = p.Id,
+                IdCliente = p.IdCliente,
+                ValorTotal = p.ValorTotal,
+                Status = p.Status,
+                Itens = p.Itens
+            }).ToList();
+
+            return Ok(pedidosModelView);
         }
-
-        [HttpGet("ObterTodos")]
-        public async Task<IActionResult> GetAllPedidos(
-            int page = 1,
-            int pageSize = 10,
-            string sortBy = null,
-            bool ascending = true,
-            StatusPedido? status = null,
-            int? minTotalValue = null,
-            int? maxTotalValue = null
-        )
+        catch (Exception ex)
         {
-            var validationErrors = new ValidationErrors { Messages = new List<string>() };
-
-            if (page <= 0)
-            {
-                validationErrors.Messages.Add("O número da página deve ser maior que zero.");
-            }
-
-            if (pageSize <= 0)
-            {
-                validationErrors.Messages.Add("O tamanho da página deve ser maior que zero.");
-            }
-
-            if (sortBy != null && sortBy.ToLower() != "valortotal")
-            {
-                validationErrors.Messages.Add("O campo de ordenação deve ser 'valortotal' ou vazio.");
-            }
-
-            if (minTotalValue.HasValue && minTotalValue < 0)
-            {
-                validationErrors.Messages.Add("O valor mínimo do pedido não pode ser negativo.");
-            }
-
-            if (maxTotalValue.HasValue && maxTotalValue < 0)
-            {
-                validationErrors.Messages.Add("O valor máximo do pedido não pode ser negativo.");
-            }
-
-            if (minTotalValue.HasValue && maxTotalValue.HasValue && minTotalValue > maxTotalValue)
-            {
-                validationErrors.Messages.Add("O valor mínimo do pedido não pode ser maior que o valor máximo.");
-            }
-
-            if (validationErrors.Messages.Count > 0)
-            {
-                return BadRequest(validationErrors);
-            }
-            try
-            {
-                var pedidos = await _pedidoRepository.GetAllPedidosAsync(page, pageSize, sortBy, ascending, status, minTotalValue, maxTotalValue);
-
-                var pedidosModelView = pedidos.Select(p => new PedidoModelView
-                {
-                    Id = p.Id,
-                    IdCliente = p.IdCliente,
-                    ValorTotal = p.ValorTotal,
-                    Status = p.Status,
-                    Itens = p.Itens
-                }).ToList();
-
-                return Ok(pedidosModelView);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Erro ao obter pedidos.");
-                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro inesperado no servidor. Tente novamente mais tarde.");
-            }
+            _logger.LogError(ex, "Erro ao obter pedidos.");
+            return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro inesperado no servidor. Tente novamente mais tarde.");
         }
+    }
 
-        [HttpGet("ObterPorId/{id}")]
-        public async Task<IActionResult> GetPedidoById(int id)
+    [HttpGet("ObterPorId/{id}")]
+    public async Task<IActionResult> GetPedidoById(int id)
+    {
+        try
         {
-            try
+            var pedido = await _pedidoService.GetPedidoByIdAsync(id);
+
+            if (pedido == null)
             {
-                var pedido = await _pedidoRepository.GetPedidoByIdAsync(id);
-
-                if (pedido == null)
-                {
-                    return NotFound("Pedido não encontrado.");
-                }
-
-                var pedidoModelView = new PedidoModelView
-                {
-                    Id = pedido.Id,
-                    IdCliente = pedido.IdCliente,
-                    ValorTotal = pedido.ValorTotal,
-                    Status = pedido.Status,
-                    Itens = pedido.Itens
-                };
-
-                return Ok(pedidoModelView);
+                return NotFound("Pedido não encontrado.");
             }
-            catch (Exception ex)
+
+            var pedidoModelView = new PedidoModelView
             {
-                _logger.LogError(ex, "Erro ao obter pedido.");
-                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro inesperado no servidor. Tente novamente mais tarde.");
-            }
+                Id = pedido.Id,
+                IdCliente = pedido.IdCliente,
+                ValorTotal = pedido.ValorTotal,
+                Status = pedido.Status,
+                Itens = pedido.Itens
+            };
+
+            return Ok(pedidoModelView);
         }
-
-        [HttpPost("Cadastrar")]
-        public async Task<IActionResult> AddPedido(PedidoDTO pedidoDTO)
+        catch (Exception ex)
         {
-            try
-            {
-                var pedido = await _pedidoService.CriarPedidoAsync(pedidoDTO);
+            _logger.LogError(ex, "Erro ao obter pedido.");
+            return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro inesperado no servidor. Tente novamente mais tarde.");
+        }
+    }
 
-                var pedidoModelView = new PedidoModelView
-                {
-                    Id = pedido.Id,
-                    IdCliente = pedido.IdCliente,
-                    ValorTotal = pedido.ValorTotal,
-                    Status = pedido.Status,
-                    Itens = pedido.Itens
-                };
+    [HttpPost("Cadastrar")]
+    public async Task<IActionResult> AddPedido(PedidoDTO pedidoDTO)
+    {
+        try
+        {
+            var pedido = await _pedidoService.AddPedidoAsync(pedidoDTO);
 
-                return CreatedAtAction(nameof(GetPedidoById), new { id = pedidoModelView.Id }, pedidoModelView);
-            }
-            catch (ArgumentException ex)
+            var pedidoModelView = new PedidoModelView
             {
-                return BadRequest(ex.Message);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
-            catch (InvalidOperationException ex)
-            {
-                // Ex: Estoque insuficiente
-                return Conflict(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Erro ao cadastrar pedido.");
-                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro inesperado no servidor. Tente novamente mais tarde.");
-            }
+                Id = pedido.Id,
+                IdCliente = pedido.IdCliente,
+                ValorTotal = pedido.ValorTotal,
+                Status = pedido.Status,
+                Itens = pedido.Itens
+            };
+
+            return CreatedAtAction(nameof(GetPedidoById), new { id = pedidoModelView.Id }, pedidoModelView);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (InvalidOperationException ex)
+        {
+            // Ex: Estoque insuficiente
+            return Conflict(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Erro ao cadastrar pedido.");
+            return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro inesperado no servidor. Tente novamente mais tarde.");
         }
     }
 }
