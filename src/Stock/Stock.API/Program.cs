7ba1e31251c7b5b685bd7f174d3e240a411a8c5b
feandrade02 <fe.a.andrade@hotmail.com>
2025-11-04 22:58:14 -0300

    build: add novos recursos a stock api

--- src/Stock/Stock.API/Program.cs
+++ src/Stock/Stock.API/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
 using Stock.API.Domain.Interfaces;
+using Stock.API.Domain.Services;
 using Stock.API.Repositories;
-using Stock.Context;
+using Stock.API.Context;
+using Stock.API.Workers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +15,18 @@ builder.Services.AddDbContext<StockContext>(options =>
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Registra as dependências do seu serviço e repositório
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+builder.Services.AddHostedService<UpdateStockWorker>();
+
+// Configura a conexão com o RabbitMQ
+var rabbitMqConnectionString = builder.Configuration.GetConnectionString("RabbitMQ");
+var factory = new ConnectionFactory { Uri = new Uri(rabbitMqConnectionString) };
+var connection = await factory.CreateConnectionAsync();
+builder.Services.AddSingleton(connection);
 
 // Adiciona o serviço de Health Checks e configura a verificação do SQL Server
 builder.Services.AddHealthChecks()
